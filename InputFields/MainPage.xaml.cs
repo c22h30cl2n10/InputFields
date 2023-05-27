8e1d6eaf5de1e17d864c1a8946eaa0d9cba60191
IamMichaell <robot.228532@gmail.com>
2023-05-27 20:11:44 +0300

    Добавьте файлы проекта.

---
+++ InputFields/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using InputFields.ViewModels;
+
+
+namespace InputFields
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
---
+++ InputFields/ViewModels/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Maui.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace InputFields.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _password;
+        private bool _isPasswordHidden;
+        private string _togglePasswordButtonImage;
+
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public bool IsPasswordHidden
+        {
+            get { return _isPasswordHidden; }
+            set { SetProperty(ref _isPasswordHidden, value); }
+        }
+
+        public string TogglePasswordButtonImage
+        {
+            get { return _togglePasswordButtonImage; }
+            set { SetProperty(ref _togglePasswordButtonImage, value); }
+        }
+
+        public ICommand TogglePasswordCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            //Установка пароля в скрытое состояние по умолчанию
+            TogglePasswordCommand = new Command(TogglePasswordVisibility);
+            IsPasswordHidden = true; 
+            TogglePasswordButtonImage = "hide_eye.png";
+        }
+
+        private void TogglePasswordVisibility()
+        {
+            IsPasswordHidden = !IsPasswordHidden;
+            TogglePasswordButtonImage = IsPasswordHidden ? "hide_eye.png" : "open_eye.png";
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "", Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #endregion
+    }
+}
